9d927158719ab53f7572f176ede3ba4cd133798f
pasbone <pasbone2@gmail.com>
2019-11-26 19:29:12 +0800

    :art: 添加新方法

--- code/src/对象数组去重/index.js
+++ code/src/对象数组去重/index.js
@@ -1,21 +1,33 @@
-"use strict";
-exports.__esModule = true;
 /**
- * @description 对象数组去重
+ * @description 对象数组去重 版本1
  * @param arr 数组源
  * @param reciver 方法
  */
-function uniq(arr, reciver) {
-    var hashMap = new Map();
-    return arr.reduce(function (previous, current) {
-        var i = hashMap.get(reciver(current));
-        if (i !== void 0) {
-            previous[i] = current;
-        }
-        else {
-            hashMap.set(current.id, previous.push(current) - 1);
-        }
-        return previous;
-    }, []);
-}
-exports.uniq = uniq;
+// export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
+//   let hashMap = new Map<any, number>();
+//   return arr.reduce<T[]>((previous, current) => {
+//     const i = hashMap.get(reciver(current));
+//     if (i !== void 0) {
+//       previous[i] = current;
+//     } else {
+//       hashMap.set(current.id, previous.push(current) - 1);
+//     }
+//     return previous;
+//   }, [])
+// }
+/**
+ * @description 对象数组去重 版本2. 比较Low
+ * @param arr 数组源
+ * @param reciver 方法
+ */
+// export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
+//   let res: IPlainObject = {};
+//   let uniArr: T[] = [];
+//   arr.forEach(element => {
+//     res[reciver(element)] = element;
+//   });
+//   for (let key in res) {
+//     uniArr.push(res[key])
+//   }
+//   return uniArr;
+// }
--- code/src/对象数组去重/index.ts
+++ code/src/对象数组去重/index.ts
@@ -1,19 +1,35 @@
-
 /**
- * @description 对象数组去重
+ * @description 对象数组去重 版本1
  * @param arr 数组源
  * @param reciver 方法
  */
-export function uniq<T extends any>(arr: T[], reciver: (currentrent: T) => any) {
+// export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
 
-  let hashMap = new Map<any, number>();
-  return arr.reduce<T[]>((previous, current) => {
-    const i = hashMap.get(reciver(current));
-    if (i !== void 0) {
-      previous[i] = current;
-    } else {
-      hashMap.set(current.id, previous.push(current) - 1);
-    }
-    return previous;
-  }, [])
-}
+//   let hashMap = new Map<any, number>();
+//   return arr.reduce<T[]>((previous, current) => {
+//     const i = hashMap.get(reciver(current));
+//     if (i !== void 0) {
+//       previous[i] = current;
+//     } else {
+//       hashMap.set(current.id, previous.push(current) - 1);
+//     }
+//     return previous;
+//   }, [])
+// }
+
+/**
+ * @description 对象数组去重 版本2. 比较Low
+ * @param arr 数组源
+ * @param reciver 方法
+ */
+// export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
+//   let res: IPlainObject = {};
+//   let uniArr: T[] = [];
+//   arr.forEach(element => {
+//     res[reciver(element)] = element;
+//   });
+//   for (let key in res) {
+//     uniArr.push(res[key])
+//   }
+//   return uniArr;
+// }
