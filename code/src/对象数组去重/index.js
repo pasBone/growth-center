a91bbdfbb017eb39841a14d947f5241b85e1e3aa
pasbone <pasbone2@gmail.com>
2019-11-27 15:24:14 +0800

    :bug: 数组去重

--- code/src/对象数组去重/index.js
+++ code/src/对象数组去重/index.js
@@ -1,20 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @description 对象数组去重 版本1
  * @param arr 数组源
  * @param reciver 方法
  */
-// export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
-//   let hashMap = new Map<any, number>();
-//   return arr.reduce<T[]>((previous, current) => {
-//     const i = hashMap.get(reciver(current));
-//     if (i !== void 0) {
-//       previous[i] = current;
-//     } else {
-//       hashMap.set(current.id, previous.push(current) - 1);
-//     }
-//     return previous;
-//   }, [])
-// }
+function uniq(arr, reciver) {
+    let hashMap = new Map();
+    return arr.reduce((previous, current) => {
+        const i = hashMap.get(reciver(current));
+        if (i !== void 0) {
+            previous[i] = current;
+        }
+        else {
+            hashMap.set(current.id, previous.push(current) - 1);
+        }
+        return previous;
+    }, []);
+}
+exports.uniq = uniq;
 /**
  * @description 对象数组去重 版本2. 比较Low
  * @param arr 数组源
@@ -31,3 +35,4 @@
 //   }
 //   return uniArr;
 // }
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBOzs7O0dBSUc7QUFDSCxTQUFnQixJQUFJLENBQWdCLEdBQVEsRUFBRSxPQUE0QjtJQUV4RSxJQUFJLE9BQU8sR0FBRyxJQUFJLEdBQUcsRUFBZSxDQUFDO0lBQ3JDLE9BQU8sR0FBRyxDQUFDLE1BQU0sQ0FBTSxDQUFDLFFBQVEsRUFBRSxPQUFPLEVBQUUsRUFBRTtRQUMzQyxNQUFNLENBQUMsR0FBRyxPQUFPLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyxPQUFPLENBQUMsQ0FBQyxDQUFDO1FBQ3hDLElBQUksQ0FBQyxLQUFLLEtBQUssQ0FBQyxFQUFFO1lBQ2hCLFFBQVEsQ0FBQyxDQUFDLENBQUMsR0FBRyxPQUFPLENBQUM7U0FDdkI7YUFBTTtZQUNMLE9BQU8sQ0FBQyxHQUFHLENBQUMsT0FBTyxDQUFDLEVBQUUsRUFBRSxRQUFRLENBQUMsSUFBSSxDQUFDLE9BQU8sQ0FBQyxHQUFHLENBQUMsQ0FBQyxDQUFDO1NBQ3JEO1FBQ0QsT0FBTyxRQUFRLENBQUM7SUFDbEIsQ0FBQyxFQUFFLEVBQUUsQ0FBQyxDQUFBO0FBQ1IsQ0FBQztBQVpELG9CQVlDO0FBRUQ7Ozs7R0FJRztBQUNILGdGQUFnRjtBQUNoRixnQ0FBZ0M7QUFDaEMsMEJBQTBCO0FBQzFCLDZCQUE2QjtBQUM3Qix1Q0FBdUM7QUFDdkMsUUFBUTtBQUNSLDJCQUEyQjtBQUMzQiw0QkFBNEI7QUFDNUIsTUFBTTtBQUNOLG1CQUFtQjtBQUNuQixJQUFJIn0=
--- code/src/对象数组去重/index.ts
+++ code/src/对象数组去重/index.ts
@@ -3,19 +3,19 @@
  * @param arr 数组源
  * @param reciver 方法
  */
-// export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
+export function uniq<T extends any>(arr: T[], reciver: (current: T) => any) {
 
-//   let hashMap = new Map<any, number>();
-//   return arr.reduce<T[]>((previous, current) => {
-//     const i = hashMap.get(reciver(current));
-//     if (i !== void 0) {
-//       previous[i] = current;
-//     } else {
-//       hashMap.set(current.id, previous.push(current) - 1);
-//     }
-//     return previous;
-//   }, [])
-// }
+  let hashMap = new Map<any, number>();
+  return arr.reduce<T[]>((previous, current) => {
+    const i = hashMap.get(reciver(current));
+    if (i !== void 0) {
+      previous[i] = current;
+    } else {
+      hashMap.set(current.id, previous.push(current) - 1);
+    }
+    return previous;
+  }, [])
+}
 
 /**
  * @description 对象数组去重 版本2. 比较Low
